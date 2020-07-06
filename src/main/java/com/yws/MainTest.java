5d77536a9901599369b947b72d0a3f20b3b99633
yws <yws528819@163.com>
2020-07-06 17:02:31 +0800

    include exclude详解...

--- src/main/java/com/yws/MainTest.java
+++ src/main/java/com/yws/MainTest.java
@@ -1,15 +1,26 @@
 package com.yws;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.yws.bean.Person;
+import com.yws.config.MyConfig;
 
 public class MainTest {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+//		Person person = applicationContext.getBean(Person.class);
+//		System.out.println(person);
+		
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyConfig.class);
 		Person person = applicationContext.getBean(Person.class);
 		System.out.println(person);
+		
+		String[] namesForType = applicationContext.getBeanNamesForType(Person.class);
+		for (String name : namesForType) {
+			System.out.println(name);
+		}
 	}
 }
