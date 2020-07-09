d918b4f6d0148f53290714de0dd68b99c2b1e0f0
yws <yws528819@163.com>
2020-07-09 08:04:16 +0800

    @Import test

--- src/test/java/com/yws/test/IOCTest.java
+++ src/test/java/com/yws/test/IOCTest.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.yws.bean.Color;
+import com.yws.bean.Person;
 import com.yws.config.MyConfig;
+import com.yws.config.MyConfig2;
 
 @SuppressWarnings("resource")
 public class IOCTest {
@@ -18,5 +21,26 @@ public class IOCTest {
 			System.out.println(bean);
 		}
 		
+	}
+	
+	
+	@Test
+	public void testImport() {
+		ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig2.class);
+		print(context);
+		Object bean1 = context.getBean("colorFactoryBean");
+		Object bean2 = context.getBean("colorFactoryBean");
+		
+		System.out.println(bean1 == bean2);
+		
+		System.out.println(context.getBean("&colorFactoryBean"));
+
+	}
+	
+	void print(ApplicationContext context) {
+		String[] names = context.getBeanDefinitionNames();
+		for (String name : names) {
+			System.out.println(name);
+		}
 	}
 }
